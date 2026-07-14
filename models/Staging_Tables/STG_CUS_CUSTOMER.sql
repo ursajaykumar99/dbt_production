7a48fe00dc6df9f452becdb7fd0c7e7cea71aa5b
Ajay Kumar Karri <pravallikaseru123@gmail.com>
2026-07-14 17:26:17 +0000

    Updated Staging Customer model with meta data

--- models/Staging_Tables/STG_CUS_CUSTOMER.sql
+++ models/Staging_Tables/STG_CUS_CUSTOMER.sql
@@ -1,4 +1,25 @@
-{{ config( materialized='table' )}}
-
-select *,
+{{  config( 
+            materialized='incremental',
+            unique_key='C_CUSTKEY',
+            incremental_strategy='merge'
+         )
+}}
+select  *,
+        SHA2(COALESCE(C_CUSTKEY,''),512)   AS EDW_LOAD_KEY_HASH,
+        SHA2(
+                CONCAT_WS('|',
+                COALESCE(C_CUSTKEY,''),
+                COALESCE(C_NAME,''),
+                COALESCE(C_ADDRESS,''),
+                COALESCE(C_NATIONKEY,''),
+                COALESCE(C_PHONE,''),
+                COALESCE(C_ACCTBAL,''),
+                COALESCE(C_MKTSEGMENT,''),
+                COALESCE(C_COMMENT,'')
+            ),
+            512
+        ) AS EDW_ROW_KEY_HASH,
+        CURRENT_TIMESTAMP() AS EDW_CREATED_TIMESTAMP,
+        CURRENT_TIMESTAMP() AS EDW_UPDATED_TIMESTAMP,
+        CURRENT_TIMESTAMP() AS EDW_LOAD_TIMESTAMP,
 from {{ source('landing_table', 'LND_CUS_CUSTOMER')}}
